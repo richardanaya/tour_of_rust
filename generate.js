cb89eb9354a370ec5f6c239ea4fbfc3daa4e3817
Richard <Richard>
2020-05-03 11:10:16 -0700

    new pages

--- generate.js
+++ generate.js
@@ -24,7 +24,7 @@ function template(title,code,content,index,isLast){
             ${content}
             </div>
             ${code?`<div class="code">
-            <iframe width="100%" src="https://repl.it/@richardanaya/${code}?lite=true" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+            <iframe width="100%" src="${code}" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
             </div>`:`<div class="code"><center><br><br><br><br><br><img src="ferris.png"></center></div>`}
         </div>
     </body>
