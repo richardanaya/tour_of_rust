06b19bcc53b86da3fdd2490cb88c72bb658f06ab
Richard <Richard>
2020-06-23 22:10:34 -0700

    new ferris

--- generate.js
+++ generate.js
@@ -64,7 +64,7 @@ function template(lessons,lang,title,code,content,index,isLast, words, is_beta){
             </div>
             ${code?`<div class="code">
             <iframe width="100%" src="${code}" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
-            </div>`:`<div class="code"><center><br><br><br><br><br><img src="ferris.png"><br><br><br><br><br></center></div>`}
+            </div>`:`<div class="code"><center><br><br><br><br><br><img src="ferris_lofi.png"><br><br><br><br><br></center></div>`}
         </div>
     </body>
     <script src="/tour.js"></script>
