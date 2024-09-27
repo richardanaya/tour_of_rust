12e45386f52fa21a0a8ce28619b6f790c982b24d
Richard Anaya <richard.anaya@gmail.com>
2024-09-27 07:50:22 -0700

    fix

--- generate.js
+++ generate.js
@@ -1,10 +1,11 @@
-const showdown = require('showdown');
-const fs = require('fs');
-const yaml = require('js-yaml');
+const showdown = require("showdown");
+const fs = require("fs");
+const yaml = require("js-yaml");
 
 const lessonSource = process.argv[2];
 const targetDir = process.argv[3];
-const generateBetaContent = process.argv.length >= 4 && process.argv[4] === 'beta';
+const generateBetaContent =
+  process.argv.length >= 4 && process.argv[4] === "beta";
 
 /**
  * @param {number} num
@@ -12,7 +13,7 @@ const generateBetaContent = process.argv.length >= 4 && process.argv[4] === 'bet
  */
 function pad(num) {
   const s = `${num}`;
-  return s.padStart(2, '0');
+  return s.padStart(2, "0");
 }
 
 /**
@@ -23,8 +24,8 @@ function pad(num) {
  * @returns {string}
  */
 function getFileName(lang, i, isBeta, chapter) {
-  if (i === 0 && lang === 'en') {
-    return 'index.html';
+  if (i === 0 && lang === "en") {
+    return "index.html";
   }
   let fileName = `${pad(i)}_${lang}.html`;
   if (chapter !== undefined && chapter !== null) {
@@ -37,29 +38,32 @@ function getFileName(lang, i, isBeta, chapter) {
  * @param {string} source
  * @returns {string[]}
  */
-const getDirectories = (source) => fs.readdirSync(source, { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
 /**
  * @param {string} path
  * @returns {any}
  */
-const getYaml = (path) => yaml.safeLoad(fs.readFileSync(path));
+const getYaml = (path) => yaml.load(fs.readFileSync(path));
 
 const languages = getDirectories(lessonSource);
 const commonWords = {};
 const chapters = [];
 
 languages.forEach((lang) => {
-//   const lang = languages[x];
+  //   const lang = languages[x];
   const langDir = `${lessonSource}/${lang}`;
   commonWords[lang] = getYaml(`${langDir}/common_words.yaml`);
-  const languageFiles = fs.readdirSync(langDir, { withFileTypes: true })
-    .filter((f) => f.isFile() && f.name.indexOf('chapter_') === 0)
+  const languageFiles = fs
+    .readdirSync(langDir, { withFileTypes: true })
+    .filter((f) => f.isFile() && f.name.indexOf("chapter_") === 0)
     .map((f) => f.name);
   languageFiles.forEach((l) => {
-    const chap = parseInt(l.substring(8, l.indexOf('.')), 10);
+    const chap = parseInt(l.substring(8, l.indexOf(".")), 10);
     if (chapters[chap] === undefined) {
       chapters[chap] = {};
     }
@@ -108,12 +112,16 @@ function getWord(words, lang, w) {
  * @returns {string}
  */
 const getHead = (words, lang) => {
-    let rtl_langs = ['ar'];
-    let lang_dir = (rtl_langs.includes(lang))?"rtl":"ltr";
-    return `<!DOCTYPE html>
+  let rtl_langs = ["ar"];
+  let lang_dir = rtl_langs.includes(lang) ? "rtl" : "ltr";
+  return `<!DOCTYPE html>
     <html lang="${lang}" dir="${lang_dir}">
     <head>
-        <title>${getWord(words, lang, 'tor')} - Let's go on an adventure!</title>
+        <title>${getWord(
+          words,
+          lang,
+          "tor"
+        )} - Let's go on an adventure!</title>
 
         <meta charset="UTF-8">
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
@@ -158,26 +166,79 @@ const getHead = (words, lang) => {
  * @param {boolean} isBeta
  * @returns
  */
-function template(lessonsData, lang, title, code, content, index, isLast, words, isBeta) {
+function template(
+  lessonsData,
+  lang,
+  title,
+  code,
+  content,
+  index,
+  isLast,
+  words,
+  isBeta
+) {
   return `${getHead(words, lang)}
     <body>
         <div class="tour">
             <div class="header">
-                <span class="title"><a href="${getFileName(lang, 0, isBeta, lessonsData[0]?.chapter)}">${getWord(words, lang, 'tor')}</a></span>
+                <span class="title"><a href="${getFileName(
+                  lang,
+                  0,
+                  isBeta,
+                  lessonsData[0]?.chapter
+                )}">${getWord(words, lang, "tor")}</a></span>
                 <span class="nav">
-                <span class="toc"><a href="TOC_${lang}.html">${getWord(words, lang, 'toc')}</a></span>
+                <span class="toc"><a href="TOC_${lang}.html">${getWord(
+    words,
+    lang,
+    "toc"
+  )}</a></span>
             </div>
             <div class="page">
             <h1>${title}</h1>
             ${content}
             <div class="bottomnav">
-                ${index !== 0 ? `<span class="back"><a href="${isBeta ? 'beta_' : ''}${getFileName(lang, index - 1, isBeta, lessonsData[index - 1]?.chapter)}" rel="prev">❮ ${getWord(words, lang, 'previous')}</a></span>` : ''}
-                ${isLast ? '' : `<span class="next"><a href="${isBeta ? 'beta_' : ''}${getFileName(lang, index + 1, isBeta, lessonsData[index + 1]?.chapter)}" rel="next">${getWord(words, lang, 'next')} ❯</a></span>`}
+                ${
+                  index !== 0
+                    ? `<span class="back"><a href="${
+                        isBeta ? "beta_" : ""
+                      }${getFileName(
+                        lang,
+                        index - 1,
+                        isBeta,
+                        lessonsData[index - 1]?.chapter
+                      )}" rel="prev">❮ ${getWord(
+                        words,
+                        lang,
+                        "previous"
+                      )}</a></span>`
+                    : ""
+                }
+                ${
+                  isLast
+                    ? ""
+                    : `<span class="next"><a href="${
+                        isBeta ? "beta_" : ""
+                      }${getFileName(
+                        lang,
+                        index + 1,
+                        isBeta,
+                        lessonsData[index + 1]?.chapter
+                      )}" rel="next">${getWord(
+                        words,
+                        lang,
+                        "next"
+                      )} ❯</a></span>`
+                }
             </div>
             </div>
-            ${code ? `<div class="code">
+            ${
+              code
+                ? `<div class="code">
             <iframe width="100%" src="${code}" scrolling="no" frameborder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals" title="Rust Playground" loading="lazy"></iframe>
-            </div>` : '<div class="code"><center><img src="/ferris_lofi.png" alt="Mascot Ferris" width="300" height="236"></center></div>'}
+            </div>`
+                : '<div class="code"><center><img src="/ferris_lofi.png" alt="Mascot Ferris" width="300" height="236"></center></div>'
+            }
         </div>
     </body>
 </html>`;
@@ -196,11 +257,13 @@ languages.forEach((lang) => {
 
   langLessons.forEach((lesson, i) => {
     let fileName = getFileName(lang, i, false, lesson?.chapter);
-    if (i === 0 && lang === 'en') {
-      fileName = 'index.html';
+    if (i === 0 && lang === "en") {
+      fileName = "index.html";
     }
 
-    let lessonTitle = `[${getWord(words, lang, 'untranslated')}] ${lesson.en.title}`;
+    let lessonTitle = `[${getWord(words, lang, "untranslated")}] ${
+      lesson.en.title
+    }`;
     let lessonContent = converter.makeHtml(lesson.en.content_markdown);
     let lessonCode = lesson.en.code;
     if (lesson[lang]) {
@@ -224,7 +287,20 @@ languages.forEach((lang) => {
       }
     }
 
-    fs.writeFileSync(`${targetDir}/${fileName}`, template(langLessons, lang, lessonTitle, lessonCode, lessonContent, c, i === langLessons.length - 1, words, false));
+    fs.writeFileSync(
+      `${targetDir}/${fileName}`,
+      template(
+        langLessons,
+        lang,
+        lessonTitle,
+        lessonCode,
+        lessonContent,
+        c,
+        i === langLessons.length - 1,
+        words,
+        false
+      )
+    );
     c += 1;
   });
 
@@ -238,7 +314,9 @@ languages.forEach((lang) => {
       }
       const fileName = getFileName(lang, i, true, lesson.chapter);
 
-      let lessonTitle = `[${getWord(words, targetLang, 'untranslated')}] ${lesson.en.title}`;
+      let lessonTitle = `[${getWord(words, targetLang, "untranslated")}] ${
+        lesson.en.title
+      }`;
       let lessonContent = lesson.en.content_markdown;
       let lessonCode = lesson.en.code;
       if (lesson[targetLang]) {
@@ -250,38 +328,76 @@ languages.forEach((lang) => {
         lessonContent = content;
         lessonCode = lesson[targetLang].code || lesson.en.code;
       }
-      fs.writeFileSync(`${targetDir}/beta_${fileName}`, template(betaLessons, lang, lessonTitle, lessonCode, lessonContent, c, i === betaLessons.length - 1, words, true));
+      fs.writeFileSync(
+        `${targetDir}/beta_${fileName}`,
+        template(
+          betaLessons,
+          lang,
+          lessonTitle,
+          lessonCode,
+          lessonContent,
+          c,
+          i === betaLessons.length - 1,
+          words,
+          true
+        )
+      );
       c += 1;
     }
   });
 
   const fileName = `TOC_${lang}.html`;
-  fs.writeFileSync(`${targetDir}/${fileName}`,
+  fs.writeFileSync(
+    `${targetDir}/${fileName}`,
     `${getHead(words, lang)}
     <body>
         <div class="tour">
             <div class="header">
-                <span class="title"><a href="${getFileName(lang, 0)}">${getWord(words, lang, 'tor')}</a></span>
+                <span class="title"><a href="${getFileName(lang, 0)}">${getWord(
+      words,
+      lang,
+      "tor"
+    )}</a></span>
                 <span class="nav">
                 </span>
             </div>
             <div>
-            <h1>${getWord(words, lang, 'lessons')}</h1>
+            <h1>${getWord(words, lang, "lessons")}</h1>
             <ul>
-        ${langLessons.map((x, i) => {
-    let targetLang = lang;
-    if (x[lang] && x[lang].clone) {
-      targetLang = x[lang].clone;
-    }
-    let s = `<li><a href="${getFileName(lang, i, false, x.chapter)}">${x[targetLang] ? x[targetLang].title : `[${getWord(words, targetLang, 'untranslated')}] ${x.en.title}`}</a></li>`;
-    if (x.chapter !== undefined) {
-      s = `</ul><h3><a href="${getFileName(lang, i, false, x.chapter)}">${x[targetLang] ? x[targetLang].title : `[${getWord(words, targetLang, 'untranslated')}] ${x.en.title}`}</a></h3><ul>`;
-    }
-    return s;
-  }).join('\n')}
+        ${langLessons
+          .map((x, i) => {
+            let targetLang = lang;
+            if (x[lang] && x[lang].clone) {
+              targetLang = x[lang].clone;
+            }
+            let s = `<li><a href="${getFileName(lang, i, false, x.chapter)}">${
+              x[targetLang]
+                ? x[targetLang].title
+                : `[${getWord(words, targetLang, "untranslated")}] ${
+                    x.en.title
+                  }`
+            }</a></li>`;
+            if (x.chapter !== undefined) {
+              s = `</ul><h3><a href="${getFileName(
+                lang,
+                i,
+                false,
+                x.chapter
+              )}">${
+                x[targetLang]
+                  ? x[targetLang].title
+                  : `[${getWord(words, targetLang, "untranslated")}] ${
+                      x.en.title
+                    }`
+              }</a></h3><ul>`;
+            }
+            return s;
+          })
+          .join("\n")}
             </ul>
             </div>
         </div>
     </body>
-    </html>`);
+    </html>`
+  );
 });
